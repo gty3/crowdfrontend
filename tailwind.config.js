61049b64f1de299e29d4b76498c32020b001caf5
gty <gefyoung@gmail.com>
2022-12-11 17:57:31 +1100

    duorepo

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./app/**/*.{js,ts,jsx,tsx}",
+      ],
+    },
+  },
+  plugins: [],
+}
